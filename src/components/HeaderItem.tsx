efb1713d497895211a5a859bfcc49a3cd14c7b41
DevWonny <cjfdnjs1994@naver.com>
2025-09-02 09:59:03 +0900

    Header Item Publishing

--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -1,9 +1,16 @@
 "use client";
-
 export default function HeaderItem() {
   return (
-    <div className="header-item-container w-24">
-      <p>Header Item</p>
+    <div className="header-item-container">
+      <div className="top-container flex justify-between text-sm">
+        <h1>Name</h1>
+        <h2>Last Price</h2>
+      </div>
+
+      <div className="bot-container text-center text-2xl">
+        {/* //! 증가일때는 녹색, 감소일때는 빨간색으로  */}
+        <p>Percentage</p>
+      </div>
     </div>
   );
 }
