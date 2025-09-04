6a580f0f40ea13bd63970fc6095c76af91d73e25
DevWonny <cjfdnjs1994@naver.com>
2025-09-04 11:18:46 +0900

    Stock Item Component Publishing

--- src/components/StockItem.tsx
+++ src/components/StockItem.tsx
@@ -1,9 +1,15 @@
 "use client";
+// style
+import "@/styles/components/StockItem.scss";
 
 export default function StockItem() {
   return (
-    <div className="stock-item-container w-50">
-      <p>Stock Item</p>
+    <div className="stock-item-container w-100 flex text-sm">
+      {/* Name | Last Price | Change Price | Change Price Percentage */}
+      <p className="stock-name">Name</p>
+      <p className="last-price">Last Price</p>
+      <p className="change-price">Change Price</p>
+      <p className="percentage">Change Percentage</p>
     </div>
   );
 }
