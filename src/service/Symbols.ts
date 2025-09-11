3a6b4221534606d1501dc20d7991ed9e59083aa5
DevWonny <cjfdnjs1994@naver.com>
2025-09-11 13:51:04 +0900

    50개 기업 정리 및 필터링

--- src/service/Symbols.ts
+++ src/service/Symbols.ts
@@ -1,4 +1,11 @@
 import axios from 'axios'
+
+export const TOP50_TICKERS = [
+  'AAPL', 'MSFT', 'GOOGL', 'GOOG', 'AMZN', 'TSLA', 'META', 'NVDA', 'BRK.B',
+  'JPM', 'JNJ', 'V', 'PG', 'XOM', 'UNH', 'MA', 'HD', 'CVX', 'AVGO', 'PFE', 'MRK', 'ABBV', 'KO',
+  'COST', 'DIS', 'WMT', 'BAC', 'INTC', 'CSCO', 'ORCL', 'NFLX', 'TSM', 'NKE', 'MCD', 'VZ', 'PEP',
+  'CRM', 'ADBE', 'IBM', 'AMD', 'QCOM', 'TXN', 'CAT', 'HON', 'GS', 'AMGN', 'LLY', 'BMY', 'UPS', 'MS'
+]
 export async function symbolList() {
   const key = process.env.FINNHUB_TOKEN;
   if (!key) {
@@ -8,11 +15,9 @@ export async function symbolList() {
   try {
     const res = await axios.get(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${key}`)
 
-    // console.log("🚀 ~ symbolList ~ res:", res)
-    const test = res.data.filter((item: any) => item.symbol === 'NVDA' || item.symbol === 'AAPL')
-    console.log("🚀 ~ symbolList ~ test:", test)
+    const filterData = res.data.filter((item: any) => TOP50_TICKERS.includes(item.symbol))
 
-    return res.data;
+    return filterData;
   } catch (err) {
     console.log("🚀 ~ symbolList ~ err:", err);
     return
@@ -21,102 +26,53 @@ export async function symbolList() {
 };
 
 // ! 미국 증시 주요 50개 회사 -> 필터링 걸어야 함...
-// AAPL – Apple
-
-// MSFT – Microsoft
-
-// GOOGL – Alphabet(Google, Class A)
-
-// GOOG – Alphabet(Google, Class C)
-
-// AMZN – Amazon
-
-// TSLA – Tesla
-
-// META – Meta Platforms(Facebook)
-
-// NVDA – NVIDIA
-
-// BRK.B – Berkshire Hathaway
-
-// JPM – JPMorgan Chase
-
-// JNJ – Johnson & Johnson
-
-// V – Visa
-
-// PG – Procter & Gamble
-
-// XOM – Exxon Mobil
-
-// UNH – UnitedHealth Group
-
-// MA – Mastercard
-
-// HD – Home Depot
-
-// CVX – Chevron
-
-// AVGO – Broadcom
-
-// PFE – Pfizer
-
-// MRK – Merck & Co.
-
-//   ABBV – AbbVie
-
-// KO – Coca - Cola
-
-// PEPSI – PepsiCo(PEP)
-
-// COST – Costco
-
-// DIS – Disney
-
-// WMT – Walmart
-
-// BAC – Bank of America
-
-// INTEL – Intel(INTC)
-
-// CSCO – Cisco Systems
-
-// ORCL – Oracle
-
-// NFLX – Netflix
-
-// TSM – Taiwan Semiconductor(ADR)
-
-// NKE – Nike
-
-// MCD – McDonald’s
-
-// VZ – Verizon
-
-// PEP – PepsiCo
-
-// CRM – Salesforce
-
-// ADBE – Adobe
-
-// IBM – IBM
-
-// AMD – Advanced Micro Devices
-
-// QCOM – Qualcomm
-
-// TXN – Texas Instruments
-
-// CAT – Caterpillar
-
-// HON – Honeywell
-
-// GS – Goldman Sachs
-
-// AMGN – Amgen
-
-// LLY – Eli Lilly
-
-// BMY – Bristol - Myers Squibb
-
-// UPS – United Parcel Service
+// AAPL – Apple O
+// MSFT – Microsoft O
+// GOOGL – Alphabet(Google, Class A) O
+// GOOG – Alphabet(Google, Class C) O
+// AMZN – Amazon O
+// TSLA – Tesla O
+// META – Meta Platforms(Facebook) O
+// NVDA – NVIDIA O
+// BRK.B – Berkshire Hathaway O
+// JPM – JPMorgan Chase O
+// JNJ – Johnson & Johnson O
+// V – Visa O
+// PG – Procter & Gamble O
+// XOM – Exxon Mobil O
+// UNH – UnitedHealth Group o
+// MA – Mastercard o
+// HD – Home Depot o
+// CVX – Chevron o
+// AVGO – Broadcom o
+// PFE – Pfizer o
+// MRK – Merck & Co. o
+//   ABBV – AbbVie o
+// KO – Coca - Cola o
+// COST – Costco o
+// DIS – Disney o
+// WMT – Walmart o
+// BAC – Bank of America o
+// INTC – Intel(INTC) o
+// CSCO – Cisco Systems o
+// ORCL – Oracle o
+// NFLX – Netflix o
+// TSM – Taiwan Semiconductor(ADR) o
+// NKE – Nike o
+// MCD – McDonald’s o
+// VZ – Verizon o
+// PEP – PepsiCo o
+// CRM – Salesforce o
+// ADBE – Adobe o
+// IBM – IBM o
+// AMD – Advanced Micro Devices o
+// QCOM – Qualcomm o
+// TXN – Texas Instruments o
+// CAT – Caterpillar o
+// HON – Honeywell o
+// GS – Goldman Sachs o
+// AMGN – Amgen o
+// LLY – Eli Lilly o
+// BMY – Bristol - Myers Squibb o
+// UPS – United Parcel Service o
+// MS – Morgan Stanley
