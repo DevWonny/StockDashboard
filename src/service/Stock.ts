61d92f2dd134e164bff634146d49eca4e4192e65
DevWonny <cjfdnjs1994@naver.com>
2025-09-12 11:52:38 +0900

    주요 API Service 로직 구현

--- src/service/Stock.ts
+++ src/service/Stock.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
 const key = process.env.FINNHUB_TOKEN;
-export async function company(symbol: string) {
-  // * Symbol
 
-
+const checkKey = () => {
   if (!key) {
     throw new Error('Service Stock Company Error!');
   }
+}
 
+export async function company(symbol: string) {
+  checkKey();
   try {
     const res = await axios.get(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${key}`)
     return res.data;
@@ -18,15 +19,38 @@ export async function company(symbol: string) {
   }
 }
 
-export async function financial() {
+export async function financial(symbol: string) {
   // * Symbol
-
+  checkKey();
+  try {
+    const res = await axios.get(`https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&token=${key}`)
+    return res.data;
+  } catch (err) {
+    console.log("🚀 ~ financial ~ err:", err)
+    return;
+  }
 }
 
-export async function surprises() {
+export async function surprises(symbol: string) {
   // * Symbol
+  checkKey();
+  try {
+    const res = await axios.get(`https://finnhub.io/api/v1/stock/earnings?symbol=${symbol}&token=${key}`)
+    return res.data;
+  } catch (err) {
+    console.log("🚀 ~ surprises ~ err:", err)
+    return;
+  }
 }
 
-export async function quote() {
+export async function quote(symbol: string) {
   // * Symbol
+  checkKey();
+  try {
+    const res = await axios.get(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${key}`)
+    return res.data;
+  } catch (err) {
+    console.log("🚀 ~ quote ~ err:", err);
+    return;
+  }
 }
