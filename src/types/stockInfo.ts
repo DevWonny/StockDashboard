7887f61a126289d4b78e20457477c6bd41d6fb73
DevWonny <cjfdnjs1994@naver.com>
2025-09-15 10:57:21 +0900

    Stock Info Types - Company Interface 정의

---
+++ src/types/stockInfo.ts
@@ -0,0 +1,25 @@
+export interface Company {
+  country: string, // 국가
+  currency: string, // 통화
+  exchange: string, // 상장 거래소
+  finnhubIndustry: string, // 산업 분류(finnhub에서 제공)
+  ipo: string, // IPO 날짜
+  logo: string,
+  marketCapitalization: number, // 시가 총액
+  name: string,
+  shareOutstanding: number, // 유통 주식 수
+  ticker: string, //상장된 거래소에서 사용하는 회사 심볼
+  weburl: string,
+}
+
+export interface Financial {
+  // 대기
+}
+
+export interface Quote {
+
+}
+
+export interface Surprises {
+
+}
